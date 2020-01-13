07e5763d483046134eebcbb1731af56b03049fa1
eden-lau <845794258@qq.com>
2020-01-13 22:26:27 +0800

    fix code
    Signed-off-by: eden-lau <845794258@qq.com>

--- src/main/java/com/maxwit/course/Client.java
+++ src/main/java/com/maxwit/course/Client.java
@@ -2,10 +2,26 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
+    public void writeFile(DataInputStream dis, DataOutputStream dos, String filename) throws IOException {
+        File file = new File(filename);
+        DataOutputStream filedos = new DataOutputStream(new FileOutputStream(file));
+
+        byte[] b = new byte[1024];
+        int i;
+
+        while ((i = dis.read(b)) > 0) {
+            filedos.write(b, 0, i);
+        }
+        dos.flush();
+        filedos.close();
+    }
+
     public static void main(String[] args) throws Exception {
+        Client client = new Client();
         String[] list = args[0].split(":");
         String path = list[0];
         int port = Integer.parseInt(list[1]);
@@ -14,27 +30,14 @@ public class Client {
         DataInputStream dis = new DataInputStream(socket.getInputStream());
         DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 
-       
         String requesString = "GET " + args[1] + " HTTP/1.1";
         dos.writeUTF(requesString);
 
         String filename = dis.readUTF();
-
-        File file = new File(filename);
-        DataOutputStream filedos = new DataOutputStream(new FileOutputStream(file));
-
-        byte[] b = new byte[1024];
-        int i;
-        double curLen = 0.0;
-        while ((i = dis.read(b)) > 0) {
-            filedos.write(b, 0, i);
-            curLen += i;
-        }
-        dos.flush();
+        client.writeFile(dis, dos, filename);
 
         dos.close();
         dis.close();
-        filedos.close();
         socket.close();
     }
 }
--- src/main/java/com/maxwit/course/Server.java
+++ src/main/java/com/maxwit/course/Server.java
@@ -2,11 +2,28 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+
+    public void readFile(DataOutputStream dos, String response, File file) throws IOException {
+
+        dos.writeBytes(response);
+        DataInputStream filedis = new DataInputStream(new FileInputStream(file));
+        byte[] b = new byte[1024];
+        int i;
+        while ((i = filedis.read(b)) > 0) {
+            dos.write(b, 0, i);
+            dos.flush();
+        }
+
+        filedis.close();
+    }
+
     public static void main(String[] args) throws Exception {
+        Server server = new Server();
         int port = Integer.parseInt(args[0]);
         ServerSocket serverSocket = new ServerSocket(port);
         Socket socket = serverSocket.accept();
@@ -19,18 +36,14 @@ public class Server {
         File file = new File("." + requesString);
 
         dos.writeUTF(file.getName());
-        String response = "HTTP/1.1 200 OK\n";
-
-        dos.writeBytes(response);
-        DataInputStream filedis = new DataInputStream(new FileInputStream(file));
-        byte[] b = new byte[1024];
-        int i;
-        while ((i = filedis.read(b)) > 0) {
-            dos.write(b, 0, i);
-            dos.flush();
+        if(file.exists()){
+            String response = "HTTP/1.1 200 OK\n";
+            server.readFile(dos, response, file);
         }
-
-        filedis.close();
+        else {
+            String response = "HTTP/1.1 404\n";
+            dos.writeBytes(response);
+        }
         dos.close();
         dis.close();
         socket.close();
